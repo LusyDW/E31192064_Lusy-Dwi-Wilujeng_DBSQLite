95bf2afd871427978da6b69b12d2c4133ca8e43b
LusyDW <lusywilujeng@gmail.com>
2021-04-26 20:17:55 +0700

    E31192064_Lusy Dwi Wilujeng_DatabaseSQLite

---
+++ DatabaseSQLite/app/src/main/java/com/lusy/databasesqlite/View_Biodata.java
@@ -0,0 +1,13 @@
+package com.lusy.databasesqlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class View_Biodata extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view__biodata);
+    }
+}
